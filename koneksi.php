72334881c4125544ecd7ae578b3a8f47a4f74dde
SudarmawanAdyS <48430387+SudarmawanAdyS@users.noreply.github.com>
2019-07-31 13:57:01 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 //$koneksi1 = mysqli_connect("localhost","root","","food");
-$koneksi1 = mysqli_connect("localhost","root",'',"minang");
+$koneksi1 = mysqli_connect("localhost","id10321363_sas123",'sas123',"id10321363_resto");
 // Check connection
 if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
